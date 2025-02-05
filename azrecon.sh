96276479d3a4e37c61075b826d18ebe1c1925925
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2025-02-04 22:39:30 -0500

    Added mode options for domain or user enumeration

--- azrecon.sh
+++ azrecon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function printhelp() {
-        echo "Usage: $0 -d DOMAIN -u USERNAME"
+        echo "Usage: $0 -m MODE -d DOMAIN -u USERNAME"
 }
 
 if [ $# -eq 0 ]; then
@@ -9,8 +9,11 @@ if [ $# -eq 0 ]; then
         exit 1
 fi
 
-while getopts "hd:u:" option; do
+while getopts "hm:d:u:" option; do
   case $option in
+    m) # mode
+      mode="$OPTARG"
+      ;;
     d) # domain
       domain="$OPTARG"
       ;;
@@ -27,6 +30,12 @@ while getopts "hd:u:" option; do
       ;;
   esac
 done
+
+if [ -z $mode ]; then
+        echo "mode required, valid options: domain, user"
+        printhelp
+        exit 1
+fi
 
 if [ -z $domain ]; then
         echo "domain required"
@@ -47,42 +56,45 @@ touch az-recon-$domain-$timestamp.txt
 echo "RealmInfo:"
 echo $realminfo >> az-recon-$domain-$timestamp.txt
 echo "Parsed realm info:" >> az-recon-$domain-$timestamp.txt
-domainname=$(echo $realminfo | grep -oE '<DomainName>(.*)<\/DomainName>' | cut -d "><" -f 3)
+domainname=$(echo $realminfo | grep -oE '<DomainName>(.*)<\/DomainName>' | cut -d "<" -f 2 | cut -d ">" -f 2)
 echo "DomainName=$domainname" | tee -a az-recon-$domain-$timestamp.txt
-namespacetype=$(echo $realminfo | grep -oE '<NameSpaceType>(.*)<\/NameSpaceType>' | cut -d "><" -f 3)
+namespacetype=$(echo $realminfo | grep -oE '<NameSpaceType>(.*)<\/NameSpaceType>' | cut -d "<" -f 2 | cut -d ">" -f 2)
 echo "NameSpaceType=$namespacetype" | tee -a az-recon-$domain-$timestamp.txt
-fedbrandname=$(echo $realminfo | grep -oE '<FederationBrandName>(.*)<\/FederationBrandName>' | cut -d "><" -f 3)
+fedbrandname=$(echo $realminfo | grep -oE '<FederationBrandName>(.*)<\/FederationBrandName>' | cut -d "<" -f 2 | cut -d ">" -f 2)
 echo "FedBrandName=$fedbrandname" | tee -a az-recon-$domain-$timestamp.txt
-if [ $namespacetype == "Federated" ]; then
-        authurl=$(echo $realminfo | grep -oE '<AuthURL>(.*)<\/AuthURL>' | cut -d "><" -f 3)
-        echo "AuthURL=$authurl" | tee -a az-recon-$domain-$timestamp.txt
-        stsauthurl=$(echo $realminfo | grep -oE '<STSAuthURL>(.*)<\/STSAuthURL>' | cut -d "><" -f 3)
-        echo "STSAuthURL=$stsauthurl" | tee -a az-recon-$domain-$timestamp.txt
+if [ "$namespacetype" == "Federated" ]; then
+	authurl=$(echo $realminfo | grep -oE '<AuthURL>(.*)<\/AuthURL>' | cut -d "<" -f 2 | cut -d ">" -f 2)
+	echo "AuthURL=$authurl" | tee -a az-recon-$domain-$timestamp.txt
+	stsauthurl=$(echo $realminfo | grep -oE '<STSAuthURL>(.*)<\/STSAuthURL>' | cut -d "<" -f 2 | cut -d ">" -f 2)
+	echo "STSAuthURL=$stsauthurl" | tee -a az-recon-$domain-$timestamp.txt
 fi
 
-if [ $namespacetype == "Managed" ]; then
-	if [ -z $username ]; then
-		echo "No username provided, domain is Managed so user enumeration may be possible" | tee -a az-recon-$domain-$timestamp.txt
-	else
-		echo "Username provided, checking $username@$domain" | tee -a az-recon-$domain-$timestamp.txt
-		usercheck=$(curl -s -X POST "https://login.microsoftonline.com/common/GetCredentialType" --data "{\"Username\":\"$username@$domain\"}")
-		echo "GetCredentialType:" >> az-recon-$domain-$timestamp.txt
-		echo $usercheck >> az-recon-$domain-$timestamp.txt
-		ifuserexists=$(echo $usercheck | grep -oE "IfExistsResult\":[0-9]," | cut -d ":," -f 2)
-		echo "IfUserExists=$ifuserexists" | tee -a az-recon-$domain-$timestamp.txt
-		# The following may not be accurate for federated domains
-		# 1 - Does not exist in tenant
-		# 0 - Exists in tenant, auth via Azure
-		if [ $ifuserexists -eq 0 ]; then
-				echo "User $username exists in $domain ($tenantid)" | tee -a az-recon-$domain-$timestamp.txt
-		elif [ $ifuserexists -eq 1 ]; then
-				echo "User $username does NOT exist in $domain ($tenantid)" | tee -a az-recon-$domain-$timestamp.txt
+# Do we need to run user enumeration?
+if [ $mode == "user" ]; then
+	if [ $namespacetype == "Managed" ]; then
+		if [ -z $username ]; then
+			echo "No username provided, domain is Managed so user enumeration may be possible" | tee -a az-recon-$domain-$timestamp.txt
 		else
+			echo "Username provided, checking $username@$domain" | tee -a az-recon-$domain-$timestamp.txt
+			usercheck=$(curl -s -X POST "https://login.microsoftonline.com/common/GetCredentialType" --data "{\"Username\":\"$username@$domain\"}")
+			echo "GetCredentialType:" >> az-recon-$domain-$timestamp.txt
+			echo $usercheck >> az-recon-$domain-$timestamp.txt
+			ifuserexists=$(echo $usercheck | grep -oE "IfExistsResult\":[0-9]," | cut -d ":" -f 2 | cut -d "," -f 1)
+			echo "IfUserExists=$ifuserexists" | tee -a az-recon-$domain-$timestamp.txt
+			# The following may not be accurate for federated domains
+			# 1 - Does not exist in tenant
+			# 0 - Exists in tenant, auth via Azure
+			if [ $ifuserexists == 0 ]; then
+				echo "User $username exists in $domain ($tenantid)" | tee -a az-recon-$domain-$timestamp.txt
+			elif [ $ifuserexists -eq 1 ]; then
+				echo "User $username does NOT exist in $domain ($tenantid)" | tee -a az-recon-$domain-$timestamp.txt
+			else
 				echo "Unmapped result: $ifuserexists; User $username might not exist in $domain ($tenantid)" | tee -a az-recon-$domain-$timestamp.txt
+			fi
 		fi
+	else
+		echo "Domain is not 'Managed', skipping user enumeration checks" | tee -a az-recon-$domain-$timestamp.txt
 	fi
-else
-    echo "Domain is not 'Managed', skipping user enumeration checks" | tee -a az-recon-$domain-$timestamp.txt
 fi
 
 echo "Done."
